63dae580e06599d74224a85a628c63f5d809bcac
Wangui <elswangui@gmail.com>
2024-06-26 18:21:28 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+
+let employee = {
+    name: "Elsie Wanjihia",
+    streetAddress : "Nairobi"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+   const updatedEmployee = {...employee}
+   updatedEmployee[key] = value
+   return updatedEmployee
+    
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; 
+    return employee; 
+}
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = Object.assign({}, employee);
+    delete newEmployee[key];
+    return newEmployee;
+  }
+
+
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
